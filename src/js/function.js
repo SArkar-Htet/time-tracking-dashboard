05527ece2abf163710e1d734aea13c126c89c72f
Soe Arkar Htet <developer.soearkarhtet@gmail.com>
2021-11-17 19:25:17 +0630

    feat: add hover state

--- src/js/function.js
+++ src/js/function.js
@@ -32,15 +32,17 @@ fetch(data)
       const {current, previous} = timeframes[timeFrameSts];
       const htmlString = `
         <article id="${attrTitle}-card" class="report__card report__card--${attrTitle} card">
-          <div class="report__card__body">
-            <div class="report__card__title report__card__detail">
-              <h2 class="report__card__header text-white">${title}</h2>
-              <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg>
-            </div>
+          <div class="report__card__body card">
+            <div class="report__card__body__wrapper">
+              <div class="report__card__title report__card__detail">
+                <h2 class="report__card__header text-white">${title}</h2>
+                <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg>
+              </div>
 
-            <div class="report__card__time report__card__detail">
-              <span id="current-work" class="current-time current-time--work text-white">${current}hrs</span>
-              <span id="previous-work" class="previous previous--work text-secondary">Last <span class="previous__time-opt previous__time-opt--work">Week</span> - <span class="previous__time previous__time--work">${previous}hrs</span></span>
+              <div class="report__card__time report__card__detail">
+                <span id="current-work" class="current-time current-time--work text-white">${current}hrs</span>
+                <span id="previous-work" class="previous previous--work text-secondary">Last <span class="previous__time-opt previous__time-opt--work">Week</span> - <span class="previous__time previous__time--work">${previous}hrs</span></span>
+              </div>
             </div>
           </div>
         </article>
